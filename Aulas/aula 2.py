a9c63dc8435568f48c0d91d43faca9f09154870e
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-03 10:45:50 -0300

    Aula 2
    Variáveis e Tipos de Dados

---
+++ Aulas/aula 2.py
@@ -0,0 +1,18 @@
+# String (texto, str)
+curso = "Introdução ao wordpress"
+
+# Inteiro (integer, int)
+duração = 85
+
+# Ponto flutuante (float)
+preço = 68.99
+
+# Booleando (bool)
+gratuito = False
+
+print(curso)
+print(duração)
+print(preço)
+
+# estiver com alguma dúvida no tipo de dados usar o comando (type)
+type(curso)
