ea1dce86a9e2e3c3400a60808f0923e730102108
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-04 17:19:58 -0300

    aula 3
    anasilando condições

---
+++ Aulas/aula 3.py
@@ -0,0 +1,16 @@
+# if ou else
+
+gratuido = False
+preço = 200.00
+
+if gratuido:
+    print("Obaa! Quero aprender agora!")
+
+elif preço <= 100.00:
+    print("Quero comprar!")
+
+elif preço < 250:
+    print("Deixa eu pensar uns 5 minutinhos")
+
+else:
+    print("Quem sabe no futuro, agora não posso")
