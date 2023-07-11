e6d0c3b64127a2b0f85fc8e35c1c1a50c3bbb854
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-11 00:23:28 -0300

    aula 6
    for

---
+++ Aulas/aula 6.py
@@ -0,0 +1,12 @@
+cursos = ["Introdução ao  Wordpress", 
+          "Criador de Sites",
+          "Afiliado de Sucesso",
+          "Introdução ao Google Ads",
+          "E-commerce e Vendas Online",
+          "Desenvolvimento de Plugins para Wordpress"]
+
+for curso in cursos:
+    print(curso)
+
+for i in range(len(cursos)): #é usada para criar uma sequência de números, o começo é incluído e o final não é incluído 
+    print(cursos[i])
