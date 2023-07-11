1449d2d590ab636573ee0ce53de05487a216438c
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-07-11 00:26:20 -0300

    aula 6.1

---
+++ Aulas/alua 6(1).py
@@ -0,0 +1,12 @@
+cursos = ["Introdução ao  Wordpress", 
+          "Criador de Sites",
+          "Afiliado de Sucesso",
+          "Introdução ao Google Ads",
+          "E-commerce e Vendas Online",
+          "Desenvolvimento de Plugins para Wordpress"]
+
+i = 0
+
+while i < len(cursos):
+    print(cursos[i])
+    i = i+1
